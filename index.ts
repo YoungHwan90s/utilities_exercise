e018047b056fb32f3f3f0c51f42f270803dd72f5
Younghwan90s <younghwan1608@gmail.com>
2023-02-13 11:15:37 +0900

    utilities exercise done

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ interface Member extends User {
     myProfile객체에 존재하는 모든 프로퍼티 필수
 */
 
-const myProfile: Member = {
+const myProfile: Omit<Member, 'createdAt' | 'updatedAt' > = {
   username: "dobby",
   imgUrl: "",
   lastVisited: "1675085621467",
@@ -31,14 +31,16 @@ const myProfile: Member = {
     2. dobbyFollowers와 harryFollowers가 컴파일 되도록 하세요.
 */
 
-const dobbyFollowers: Member = [{ usrename: "harry", imgUrl: "harry.jpg" }];
-const harryFollowers: Member = [];
+const dobbyFollowers: Member['followers'] = [{ username: "harry", imgUrl: "harry.jpg" }];
+const harryFollowers: Member['followers'] = [];
 
 /*
     3. http 모듈에서 ClientRequestArgs를 import하고 host, headers, 그리고 method를 분리하세요.
 */
 
-type ClientRequestHeadersAndHost = {};
+import { ClientRequestArgs } from 'http'
+
+type ClientRequestHeadersAndHost = Pick<ClientRequestArgs, 'method' | 'host' | 'headers'>;
 
 const clientRequest: ClientRequestHeadersAndHost = {
   method: "GET",
@@ -51,33 +53,39 @@ const clientRequest: ClientRequestHeadersAndHost = {
     전환한 후 mySocketOptions을 완성하세요.
 */
 
-type NoOptionalTLSSocketOptions = {};
+import { TLSSocketOptions } from 'tls';
 
-const mySocketOptions: NoOptionalTLSSocketOptions = {
+type NoOptionalTLSSocketOptions = Required<TLSSocketOptions>;
+
+const mySocketOptions: Pick<NoOptionalTLSSocketOptions, 'isServer'> = {
   isServer: true,
-  // ...
 };
+
 
 /*
     5. net 모듈에서 SocketReadyState을 import하고 다음을 완성하세요. 새로운 타입 선언 금지.
 */
 
-const openState: Type = "open";
-const readOnlyState: Type = "readOnly";
-const closedState: Type = "closed";
+import { SocketReadState } from 'net';
+
+const openState: Extract<SocketReadState, 'open' > = "open";
+const readOnlyState: Extract<SocketReadState, 'readOnly' > = "readOnly";
+const closedState: Extract<SocketReadState, 'closed' > = "closed";
 
 /*
     6. SocketReadyState를 사용해서 다음을 충족하는 타입을 완성하고 반영하세요.
 */
 
-const allStatesExceptClosed = ["opening", "open", "readOnly", "writeOnly"]; // ✅
-const iShouldntCompile = ["opening", "open", "readOnly", "writeOnly", "closed"]; // ❌
+type newSocketReadyState = Exclude<SocketReadState, 'closed'>
+
+const allStatesExceptClosed: newSocketReadyState[] = ["opening", "open", "readOnly", "writeOnly"]; // ✅
+// const iShouldntCompile: Exclude<SocketReadState, 'closed'> = ["opening", "open", "readOnly", "writeOnly", "closed"]; // ❌
 
 /*
     7. 6번에서 완성 한 타입으로 다음을 충족하는 타입을 만드세요.
 */
 
-const states = {
+const states: Record<newSocketReadyState, string> = {
   opening: "opening",
   open: "open",
   readOnly: "readOnly",
@@ -90,39 +98,50 @@ const states = {
     그런 후, MemoryUsage의 프로퍼티 중 2개를 사용해보세요.
 */
 
-const memoryUsage = {};
+import { MemoryUsage } from 'process';
+
+const memoryUsage: Partial<MemoryUsage> = {
+  total: 1000,
+  used: 600
+};
 
 /*
     9. null과 undefined가 있는 현실적인 타입을 만들어보고, 만들어진 타입을 non-nullable 타입으로 전환하세요.
 */
 
-type CustomType = {};
+type CustomType = {
+  studentName: string
+  grade: number
+  essaySumbit: null | undefined
+  graduate: null | undefined
+};
+
+type nonNullableCustomType = {
+  [k in keyof CustomType]: NonNullable<CustomType>
+}
 
 /*
     10. 다음 함수들의 반환 타입을 유틸리티 타입을 사용해 정의해보세요.
 */
 
-function getsUsername(user: User) {
+function getsUsername(user: User): TGetUserNameReturn {
   return user.username;
 }
 
-type TGetUserNameReturn = {};
+type TGetUserNameReturn = User['username'];
 
-// ---
-
-function getUser(member: Member) {
+function getUser(member: Member): TGetUserReturn {
   return { username: member.username, imgUrl: member.imgUrl } as User;
 }
 
-type TGetUserReturn = {};
+type TGetUserReturn = Pick<Member, 'username' | 'imgUrl'>;
 
-// ---
 
-function getFollowersAndViewCount(member: Member) {
+function getFollowersAndViewCount(member: Member): TFollowerAndViewCountReturn {
   return {
     followers: member.followers,
     viewCount: member.viewCount,
   };
-}
+};
 
-type TFollowerAndViewCountReturn = {};
+type TFollowerAndViewCountReturn = Pick<Member, 'followers' | 'viewCount'>;
